261eadeecc42d222157dc66428d8e0b5d0d50a8a
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-02-19 17:18:47 -0800

    Add files via upload

---
+++ The-Tech-Academy/Project4_Dictionaries/JS/main.js
@@ -0,0 +1,23 @@
+﻿// Defines the MyCar dictionary variable with keys & values 
+var MyCar = {
+    Maker: "Honda",
+    Model: "CR-V",
+    Color: "Blue",
+    Year: 2003,
+    Owner: "Me"
+};
+
+// A function that shows MyCar's info when the "Show Information" button is clicked  
+function Show_Info() {
+    document.getElementById("Maker").innerHTML = MyCar.Maker;
+    document.getElementById("Model").innerHTML = MyCar.Model;
+    document.getElementById("Color").innerHTML = MyCar.Color;
+    document.getElementById("Year").innerHTML = MyCar.Year;
+    document.getElementById("Owner").innerHTML = MyCar.Owner;
+}
+
+// A function that deletes the Owner Key/Value from the dictionary 
+function Delete_Owner() {
+    delete MyCar.Owner;
+    document.getElementById("Owner").innerHTML = MyCar.Owner;
+}
