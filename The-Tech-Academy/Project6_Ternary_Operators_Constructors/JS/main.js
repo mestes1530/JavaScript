4c23a2bdea2d88542dc1100da5a9cc945d530ee6
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-02-22 20:58:56 -0800

    Add files via upload

---
+++ The-Tech-Academy/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -0,0 +1,23 @@
+﻿// Constructor function that creates a car object
+function Car(maker, model, color, year) {
+    this.carMaker = maker;
+    this.carModel = model;
+    this.carColor = color;
+    this.carYear = year;
+}
+
+// Function that is called when the user hits submit, creates car object and prints it 
+function Create_Car() {
+    // Gets the input values and saves them as variables  
+    var CarMaker = document.getElementById("CarMaker").value;
+    var CarModel = document.getElementById("CarModel").value;
+    var CarColor = document.getElementById("CarColor").value;
+    var CarYear = document.getElementById("CarYear").value;
+
+    // Nested call to the constructor function that creates a car based off input variables
+    var UsersCar = new Car(CarMaker, CarModel, CarColor, CarYear);
+
+    // Prints out the car object created by the user
+    document.getElementById("Result").innerHTML = "{ Car Maker: " + UsersCar.carMaker + ", Car Model: " +
+        UsersCar.carModel + ", Car Color: " + UsersCar.carColor + ", Car Year: " + UsersCar.carYear + " }";
+}
