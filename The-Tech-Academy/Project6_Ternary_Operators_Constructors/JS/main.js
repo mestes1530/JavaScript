25847a6bbf3d0f3559d9a281ab92888dc55ec8c2
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-02-22 21:31:31 -0800

    Update main.js

--- The-Tech-Academy/Project6_Ternary_Operators_Constructors/JS/main.js
+++ The-Tech-Academy/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -1,9 +1,10 @@
-﻿// Constructor function that creates a car object
-function Car(maker, model, color, year) {
+// Constructor function that creates a car object
+function Car(maker, model, color, year, fuel) {
     this.carMaker = maker;
     this.carModel = model;
     this.carColor = color;
     this.carYear = year;
+    this.carFuel = fuel;
 }
 
 // Function that is called when the user hits submit, creates car object and prints it 
@@ -13,11 +14,17 @@ function Create_Car() {
     var CarModel = document.getElementById("CarModel").value;
     var CarColor = document.getElementById("CarColor").value;
     var CarYear = document.getElementById("CarYear").value;
+    
+
+    // Ternary opperation that determines car's fuel type based off checkbox result
+    var CarElectricCheck = document.getElementById("CarFuel").value;
+    var CarFuel = (CarElectricCheck) ? "Electric" : "Gas";
 
     // Nested call to the constructor function that creates a car based off input variables
-    var UsersCar = new Car(CarMaker, CarModel, CarColor, CarYear);
+    var UsersCar = new Car(CarMaker, CarModel, CarColor, CarYear, CarFuel);
 
     // Prints out the car object created by the user
     document.getElementById("Result").innerHTML = "{ Car Maker: " + UsersCar.carMaker + ", Car Model: " +
-        UsersCar.carModel + ", Car Color: " + UsersCar.carColor + ", Car Year: " + UsersCar.carYear + " }";
+        UsersCar.carModel + ", Car Color: " + UsersCar.carColor + ", Car Year: " + UsersCar.carYear +
+        ", Car Fuel: " + UsersCar.carFuel;
 }
