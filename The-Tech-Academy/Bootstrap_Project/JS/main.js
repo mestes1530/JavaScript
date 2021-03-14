225ecc57cc4e4851b57ed74743e5cac5ecf0541d
mestes1530 <41971795+mestes1530@users.noreply.github.com>
2021-03-13 17:12:41 -0800

    Update main.js

--- The-Tech-Academy/Bootstrap_Project/JS/main.js
+++ The-Tech-Academy/Bootstrap_Project/JS/main.js
@@ -1,4 +1,9 @@
-﻿// Tooltip used by top navbar
+// Tooltip used by top navbar
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Displays popover explanation of badges 
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
 });
